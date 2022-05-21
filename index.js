36b3652f666b11ac6344db803e147b9fcd1eebb3
pranab56 <pronabhalder53@gmail.com>
2022-05-21 18:51:02 +0600

    put

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -15,11 +16,30 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+
+function jwtVeryfied(req,res,next){
+  const authorization=req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({message: "UnAuthorization"})
+  }
+  const token=authorization.split(' ')[1]
+  jwt.verify(token, process.env.ACCESS_WEB_TOKEN, function(err, decoded) {
+   
+    if(err){
+      return res.status(403).send({message : "forbidden"})}
+      req.decoded=decoded;
+      next()
+  });
+ 
+ 
+}
+
 async function run() {
   try {
     await client.connect();
     const serviceCollection = client.db("doctor-portal").collection("service");
     const bookingCollection = client.db("doctor-portal").collection("data");
+    const userCollection = client.db("doctor-portal").collection("users");
     app.get("/service", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
@@ -27,11 +47,19 @@ async function run() {
       res.send(service);
     });
     
-    app.get("/data",async (req,res)=>{
-       const query={};
-       const cursor=await bookingCollection.find(query).toArray();
-       res.send(cursor)
+    app.get("/data", jwtVeryfied, async (req,res)=>{
+      const email=req.query.email;
+      const decodedEmail=req.decoded.email;
+      if(decodedEmail===email){
+        const query={email:email}
+        const cursor=await bookingCollection.find(query).toArray();
+       return res.send(cursor)
+      }
+        
+       
+       
     })
+
 
     app.post("/data", async (req, res) => {
       const body = req.body;
@@ -75,7 +103,35 @@ async function run() {
 // -------------------------------------------------------------------------
 // -------------------------------------------------------------------------
 
+    app.get('/users', async(req,res)=>{
+      const result=await userCollection.find().toArray();
+      res.send(result)
+    })
 
+    app.put('/users/admin/:email', async(req,res)=>{
+      const email=req.params.email;
+      const filter={email:email};
+      const updateDoc = {
+        $set: {Role :'Admin'}
+        ,
+      };
+      const result=await userCollection.updateOne(filter,updateDoc);
+      res.send(result)
+    })
+
+    app.put('/users/:email',async(req,res)=>{
+      const email=req.params.email;
+      const filter={email:email};
+      const user=req.body;
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user
+        ,
+      };
+      const result=await userCollection.updateOne(filter,updateDoc,options);
+      const token=jwt.sign({email:email},process.env.ACCESS_WEB_TOKEN,{ expiresIn: '1h' })
+      res.send({result,token})
+    })
 
 
   } finally {
